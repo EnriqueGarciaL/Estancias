f4c564b9b29431b0258610e32045c2dbffa648c1
EnriqueGarciaL <1330203@upv.edu.mx>
2014-11-27 09:03:04 -0600

    update

--- hola.py
+++ hola.py
@@ -18,44 +18,23 @@ def fun3(x):
 			suma[i]+=(aux*aux).sum(axis=0)
 	return suma
 
-w=0.7
-c1=1.4
-c2=1.4
 
-gmax=2000
+def graficar(gmax,grafica):
+	plt.scatter(range(gmax),grafica)
+	plt.show()
 
-ndims=30
-nparts=40
+def ejecute(lS,lI,fun):
+	w=0.7
+	c1=1.4
+	c2=1.4
 
-lS=100
-lI=-100
+	gmax=2000
 
-x = random.random((nparts,ndims))
-x = x*(lS-lI)+lI
+	ndims=30
+	nparts=40
 
-flag1=x>=lI
-flag2=x<lS
-flag3=logical_and(flag1,flag2)
-prom=sum(x[flag3])/len(x[flag3])
-x[~flag3]=prom
-
-
-fx = fun3(x)
-pbest=x.copy()
-fx_pbest=fx.copy()
-index = argmin(fx_pbest)
-gbest=x[index].copy()
-fx_gbest=fx[index].copy()
-
-v =  zeros((nparts,ndims))
-grafica=[]
-i=0
-
-while i<gmax:
-	cSocial = c1*random.random()*(pbest-x)
-	cCognitivo = c2*random.random()*(gbest-x)
-	v= w*v + cSocial + cCognitivo
-	x = x+v
+	x = random.random((nparts,ndims))
+	x = x*(lS-lI)+lI
 
 	flag1=x>=lI
 	flag2=x<lS
@@ -63,19 +42,48 @@ while i<gmax:
 	prom=sum(x[flag3])/len(x[flag3])
 	x[~flag3]=prom
 
-	fx=fun3(x)
-	index = fx < fx_pbest
-	pbest[index] = x[index]
-	fx_pbest[index]=fx[index]
+
+	fx = fun(x)
+	pbest=x.copy()
+	fx_pbest=fx.copy()
 	index = argmin(fx_pbest)
+	gbest=x[index].copy()
+	fx_gbest=fx[index].copy()
 
-	if  fx_pbest[index] < fx_gbest:
-		gbest=x[index]
-		fx_gbest=fx[index]
+	v =  zeros((nparts,ndims))
+	grafica=[]
+	i=0
 
-	grafica.append(fx_gbest)
-	i+=1
+	while i<gmax:
+		cSocial = c1*random.random()*(pbest-x)
+		cCognitivo = c2*random.random()*(gbest-x)
+		v= w*v + cSocial + cCognitivo
+		x = x+v
 
-plt.scatter(range(gmax),grafica)
-print (gbest," : ",fx_gbest)
-plt.show()
+		flag1=x>=lI
+		flag2=x<lS
+		flag3=logical_and(flag1,flag2)
+		prom=sum(x[flag3])/len(x[flag3])
+		x[~flag3]=prom
+
+		fx=fun(x)
+		index = fx < fx_pbest
+		pbest[index] = x[index]
+		fx_pbest[index]=fx[index]
+		index = argmin(fx_pbest)
+
+		if  fx_pbest[index] < fx_gbest:
+			gbest=x[index]
+			fx_gbest=fx[index]
+
+		grafica.append(fx_gbest)
+		i+=1
+
+	graficar(gmax,grafica)
+
+def main():
+	ejecute(100,-100,fun1)
+	ejecute(10,-10,fun2)
+	ejecute(100,-100,fun3)
+
+main()
